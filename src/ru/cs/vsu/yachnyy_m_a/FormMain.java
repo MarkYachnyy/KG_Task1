1adb31bc6722cd51bb112b8fa0e91f96c71592d9
Mark_Iachnyi <markyachny@gmail.com>
2023-09-05 19:14:04 +0300

    wheel arcs added

--- src/ru/cs/vsu/yachnyy_m_a/FormMain.java
+++ src/ru/cs/vsu/yachnyy_m_a/FormMain.java
@@ -1,34 +1,41 @@
 package ru.cs.vsu.yachnyy_m_a;
 
+import ru.cs.vsu.yachnyy_m_a.graphics.Bus;
 import ru.cs.vsu.yachnyy_m_a.util.SwingUtils;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class FormMain extends JFrame{
+public class FormMain extends JFrame {
+
     private JPanel PanelMain;
     private JButton ButtonPause;
     private JSpinner SpinnerBusSpeed;
     private JPanel PanelPaintContainer;
     private JPanel PanelPaint;
 
-    public FormMain(){
+    public static final int PANEL_BORDER = 8;
+
+    public FormMain() {
         this.setContentPane(PanelMain);
         this.setTitle("Task1");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.pack();
 
-        PanelPaint = new JPanel(){
+        PanelPaint = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 Graphics2D g2d = (Graphics2D) g;
-                SwingUtils.setFixedSize(PanelPaint, PanelPaintContainer.getWidth(), PanelPaintContainer.getHeight());
-                g2d.setColor(Color.RED);
-                g2d.fillRect(0,0, PanelPaint.getWidth(), PanelPaint.getHeight());
+                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                SwingUtils.setFixedSize(PanelPaint, PanelPaintContainer.getWidth() - PANEL_BORDER, PanelPaintContainer.getHeight() - PANEL_BORDER);
+                g2d.setColor(Color.BLUE);
+                g2d.fillRect(0, 0, PanelPaint.getWidth(), PanelPaint.getHeight());
+                Bus bus = new Bus(10, PanelPaint.getWidth() / 2, PanelPaint.getHeight() / 2 + 75, Color.WHITE);
+                bus.draw(g2d);
             }
         };
 
-        PanelPaintContainer.add(PanelPaint);
+        PanelPaintContainer.add(new JScrollPane(PanelPaint));
     }
 }
--- src/ru/cs/vsu/yachnyy_m_a/graphics/Bus.java
+++ src/ru/cs/vsu/yachnyy_m_a/graphics/Bus.java
@@ -3,13 +3,60 @@ package ru.cs.vsu.yachnyy_m_a.graphics;
 import java.awt.*;
 
 public class Bus {
-    private int route;
+    private int route, x, y;
+    private Color main_color;
 
-    public Bus(int route) {
+    private static int BUS_LENGTH = 600;
+    private static int BUS_HEIGHT = 200;
+    private static int WHEEL_RADIUS = 35;
+    private static int DOOR_WIDTH = 60;
+    private static int WHEEL_ARC_WIDTH = 10;
+    private static int WHEEL_GAP = 5;
+
+    public Bus(int route, int x, int y, Color main_color) {
         this.route = route;
+        this.x = x;
+        this.y = y;
+        this.main_color = main_color;
     }
 
-    public void draw(Graphics2D g2d, int x, int y){
+    public void draw(Graphics2D g2d) {
+
+        g2d.setColor(main_color);
+        Polygon polygon1 = new Polygon();
+        polygon1.addPoint(x - BUS_LENGTH / 2, y);
+        polygon1.addPoint(x - BUS_LENGTH / 2, y - 50);
+        polygon1.addPoint(x - BUS_LENGTH / 2 + 5, y - BUS_HEIGHT);
+        polygon1.addPoint(x + BUS_LENGTH / 2 - 5, y - BUS_HEIGHT);
+        polygon1.addPoint(x + BUS_LENGTH / 2, y - BUS_HEIGHT + 50);
+        polygon1.addPoint(x + BUS_LENGTH / 2, y - 10);
+        polygon1.addPoint(x + BUS_LENGTH / 2 - 40, y);
+        g2d.fillPolygon(polygon1);
+
+        g2d.setColor(Color.BLACK);
+        g2d.fillArc(x - BUS_LENGTH / 2 + 40, y - WHEEL_RADIUS - WHEEL_GAP - WHEEL_ARC_WIDTH - 1,
+                (WHEEL_RADIUS + WHEEL_GAP + WHEEL_ARC_WIDTH + 1) * 2,
+                (WHEEL_RADIUS + WHEEL_GAP + WHEEL_ARC_WIDTH + 1) * 2, 0, 180);
+
+
+        g2d.fillArc(x + BUS_LENGTH / 2 - 40 - DOOR_WIDTH - 20 - (WHEEL_RADIUS + WHEEL_GAP + WHEEL_ARC_WIDTH + 1) * 2,
+                y - WHEEL_RADIUS - WHEEL_GAP - WHEEL_ARC_WIDTH - 1,
+                (WHEEL_RADIUS + WHEEL_GAP + WHEEL_ARC_WIDTH + 1) * 2,
+                (WHEEL_RADIUS + WHEEL_GAP + WHEEL_ARC_WIDTH + 1) * 2, 0, 180);
+
+        g2d.setColor(main_color);
+
+        g2d.fillArc(x - BUS_LENGTH / 2 + 40 + 1, y - WHEEL_RADIUS - WHEEL_GAP - WHEEL_ARC_WIDTH,
+                (WHEEL_RADIUS + WHEEL_GAP + WHEEL_ARC_WIDTH) * 2,
+                (WHEEL_RADIUS + WHEEL_GAP + WHEEL_ARC_WIDTH) * 2, 0, 180);
+
+
+        g2d.fillArc(x + BUS_LENGTH / 2 - 40 - DOOR_WIDTH - 20 - (WHEEL_RADIUS + WHEEL_GAP + WHEEL_ARC_WIDTH) * 2 - 1,
+                y - WHEEL_RADIUS - WHEEL_GAP - WHEEL_ARC_WIDTH,
+                (WHEEL_RADIUS + WHEEL_GAP + WHEEL_ARC_WIDTH) * 2,
+                (WHEEL_RADIUS + WHEEL_GAP + WHEEL_ARC_WIDTH) * 2, 0, 180);
+
+        g2d.setColor(new Color(70,70,70));
 
     }
 }
